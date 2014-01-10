70b46b5b139339c9d3cbabdad87cda08d284497c
Lars Greiss <kufikugel@googlemail.com>
2014-01-10 12:52:06 +0100

    InCallUI: Incoming Call in Background (1/3)
    Patch ported from 4.2 which was done by our lovely HCP. A
    very popular one so we bring it back and adapt it to 4.4
    
    Allows user to set that Incoming Call UI will remain in background
    if screen is on. Instead, a notification is added to StatusBar, which
    can be used to bring up the Incoming Call UI. Ringtone and vibration
    still occur as per system settings.
    
    This has the same effect as apps marked as "immersive". It prevents
    the incoming call from backgrounding the current activity.
    
    PatchSet:
    - show normal inCallUI if keyguard is currently active
    
    PS:
    - cleanup
    
    PS
    - do not try to get the services if the feature is disabled
    
    PS:
    - do not disable notificationshade and navbar if it is a background call
    
    PS:
    - derp
    
    InCallUI: add decline and answer  incoming call action on notifications
    
    Add decline and answer call action on notification if an call is incoming. Especially
    usefull for call in background feature to have the ability to directly
    decline or answer a incoming call. But as well usefull to decline/answer a second call
    during one is incomming.
    
    PS:
    - found a better way for the answer button
    
    PS:
    - fix keep flip action alive
    - fix collapse notification shade when answering call
    - answer call before starting the UI to do not have
      shown the incall screen for a brief second
    
    PS:
    - fix wrong user on sending close system dialogs
    
    Change-Id: I92d8f5a2de74f9877a61c4da4c3eaf2e25230895
    
    ----------
    
    PS2: Squash answer/decline buttons.
    
    Conflicts:
    	src/com/android/incallui/InCallPresenter.java
    
    * Squashed of above two commits, thanks Lars.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I68e432a548c10d07cc9d668782cb544fbb4d421d

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -29,6 +29,11 @@ import com.google.common.base.Preconditions;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ActivityNotFoundException;
+import android.provider.Settings;
+import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.view.IWindowManager;
 
 import com.android.services.telephony.common.Call;
 import com.android.services.telephony.common.Call.Capabilities;
@@ -88,6 +93,8 @@ public class InCallPresenter implements CallList.Listener {
                                           {0, 0, 1, 1},
                                           {0, 1, 0, 1},
                                           {0, 0, 0, 0}};
+
+    private boolean mCallUiInBackground = false;
 
     /**
      * Is true when the activity has been previously started. Some code needs to know not just if
@@ -379,8 +386,11 @@ public class InCallPresenter implements CallList.Listener {
         mInCallState = newState;
 
         // Disable notification shade and soft navigation buttons
+        // on new incoming call as long it is no background call
         if (newState.isIncoming()) {
-            CallCommandClient.getInstance().setSystemBarNavigationEnabled(false);
+            if (!mCallUiInBackground) {
+                CallCommandClient.getInstance().setSystemBarNavigationEnabled(false);
+            }
         }
 
         for (IncomingCallListener listener : mIncomingCallListeners) {
@@ -769,7 +779,38 @@ public class InCallPresenter implements CallList.Listener {
             mInCallActivity = null;
         }
 
-        mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(inCallState, mCallList);
+        // check if the user want to have the call UI in background and set it up
+        mCallUiInBackground = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.CALL_UI_IN_BACKGROUND, 0) == 1;
+
+        if (mCallUiInBackground) {
+            // get power service to check later if screen is on
+            final PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+            // check if keyguard is currently shown
+            final IWindowManager windowManagerService = IWindowManager.Stub.asInterface(
+                    ServiceManager.getService(Context.WINDOW_SERVICE));
+            boolean isKeyguardShowing = false;
+            try {
+                isKeyguardShowing = windowManagerService.isKeyguardLocked();
+            } catch (RemoteException e) {
+            }
+            mCallUiInBackground = pm.isScreenOn() && !isKeyguardShowing;
+        }
+
+        mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
+                inCallState, mCallList, mCallUiInBackground);
+    }
+
+    /**
+     * Starts the incoming call Ui immediately used by the incoming call
+     * notification sent from framework's notification mechanism
+     */
+    public void startIncomingCallUi() {
+        // Update the notification and UI this time with fullscreen intent
+        // First cancel the actual notification and then update
+        mStatusBarNotifier.cancelInCall();
+        mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
+                InCallState.INCALL, mCallList, false);
     }
 
     /**
